5d55909f8583445f5dd151d64c80bde317478b26
gskundi87 <gurpal.kundi@brightspeed.com>
2024-03-24 11:30:56 -0700

    minor changes, added some actions for Book Page

--- libs/books-page/actions/src/lib/books-api.actions.ts
+++ libs/books-page/actions/src/lib/books-api.actions.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createAction, props } from '@ngrx/store';
-import { BookModel } from '@book-co/shared-models';
+import { BookModel, BookRequiredProps } from '@book-co/shared-models';
+
+export const enterBookPage = createAction("[Books Page] Enter Books Page", props<{key: {}}>());
+
+export const createBook = createAction("[Books Page] Create a Book", props<{book: BookRequiredProps}>());
+
